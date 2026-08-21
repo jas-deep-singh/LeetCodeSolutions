184955dc4b2c45c6154407b62f9e94c89c8d3e65
Jasdeep Singh <jasdeepsingh6864@gmail.com>
2026-08-21 19:18:04 +0530

    Time: 7 ms (11.86%), Space: 53.5 MB (5.9%) - LeetHub

--- 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.java
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.java
@@ -1,17 +1,12 @@
 class Solution {
-    public boolean checkValidity(String combi) {
-        for(int i=1;i<combi.length();i++) {
-            if(combi.charAt(i)=='0' && combi.charAt(i)==combi.charAt(i-1)) {
-                return false;
+    public void findAllStrings(List<String> result, int n, String combi) {
+        if(combi.length()>=2) {
+            if(combi.charAt(combi.length()-1)=='0' && combi.charAt(combi.length()-1)==combi.charAt(combi.length()-2)) {
+                return;
             }
         }
-        return true;
-    }
-    public void findAllStrings(List<String> result, int n, String combi) {
         if(combi.length()==n) {
-            if(checkValidity(combi)) {
-                result.add(combi);
-            }
+            result.add(combi);
             return;
         }
         for(int i=0;i<2;i++) {
