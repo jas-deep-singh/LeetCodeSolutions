37fcd83c55b0d1dfdc2dd4b83a5895f597edae10
Jasdeep Singh <jasdeepsingh6864@gmail.com>
2026-08-21 18:09:21 +0530

    Time: 2 ms (87.13%), Space: 49.2 MB (54.06%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -1,12 +1,14 @@
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if(head==null || head.next==null) {
-            return head;
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr!=null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
-        ListNode newHead = reverseList(head.next);
-        head.next.next = head;
-        head.next = null;
-        return newHead;
+        return prev;
     }
     public void reorderList(ListNode head) {
         if(head==null || head.next==null) {
